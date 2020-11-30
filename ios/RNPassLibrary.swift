c5d4c60172185c519eee6e73872c9fa3f809fc77
kamaal111 <kamaal.f1@gmail.com>
2020-11-30 08:34:21 +0100

    Making methods public

--- ios/RNPassLibrary.swift
+++ ios/RNPassLibrary.swift
@@ -2,21 +2,21 @@ import PassLibrary
 import UIKit
 
 @objc(RNPassLibrary)
-class RNPassLibrary: NSObject {
+public class RNPassLibrary: NSObject {
 
     private let passLibrary = PassLibrary() // From PassLibrary
 
     @objc
-    func constantsToExport() -> [AnyHashable: Any]! {
+    public func constantsToExport() -> [AnyHashable: Any]! {
         return ["name": "RNPassLibrary"]
     }
 
     @objc
-    func getRemotePKPassAndPresentPKPassView(_ urlString: String,
-                                            resolver resolve: @escaping RCTPromiseResolveBlock,
-                                            rejecter reject: @escaping RCTPromiseRejectBlock) {
+    public func getRemotePKPassAndPresentPKPassView(_ urlString: String,
+                                                    resolver resolve: @escaping RCTPromiseResolveBlock,
+                                                    rejecter reject: @escaping RCTPromiseRejectBlock) {
         guard let pkPassUrl = URL(string: urlString) else {
-            // Handle with rejecter
+            // TODO: Handle with rejecter
             return
         }
         self.passLibrary.getRemotePKPass(from: pkPassUrl) { (result: Result<Data, Error>) in
@@ -26,7 +26,7 @@ class RNPassLibrary: NSObject {
             case .success(let pkpassData):
                 DispatchQueue.main.async {
                     guard let keyWindow = UIApplication.shared.keyWindow else {
-                        // Handle with rejecter
+                        // TODO: Handle with rejecter
                         return
                     }
                     do {
