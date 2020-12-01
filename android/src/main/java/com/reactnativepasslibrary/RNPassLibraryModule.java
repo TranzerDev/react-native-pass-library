6913500c0046fc4b903c7bf59b2df3c6ab96ea52
kamaal111 <kamaal.f1@gmail.com>
2020-12-01 22:49:09 +0100

    Hopefully Java will save the day

---
+++ android/src/main/java/com/reactnativepasslibrary/RNPassLibraryModule.java
@@ -0,0 +1,20 @@
+package com.reactnativepasslibrary;
+
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.Promise;
+
+public class RNPassLibraryModule extends ReactContextBaseJavaModule {
+
+    @Override
+    public String getName() {
+        return "RNPassLibrary";  // Name of the Native Modules.
+    }
+
+    @ReactMethod
+    public void getRemotePKPassAndPresentPKPassView(String urlString, Promise promise) {
+        promise.reject("LIBRARY_NOT_SUPPORTED", "This library is not supported on Android!");
+    }
+
+}
