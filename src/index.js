6ce393da0b1b01c2feeaca09a614d887b8c2f1bc
kamaal111 <kamaal.f1@gmail.com>
2020-12-13 14:57:25 +0100

    Stubbing functions on Android

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
-import { NativeModules } from 'react-native';
+import { NativeModules, Platform } from 'react-native';
 
 const { RNPassLibrary } = NativeModules;
 
-export default RNPassLibrary;
+const library = {
+    getRemotePKPassAndPresentPKPassView: async (url) => {
+        if (!Platform.OS === 'ios') {
+            throw 'This library is not supported on Android'
+        }
+        return await RNPassLibrary.getRemotePKPassAndPresentPKPassView(url)
+    }
+}
+export default library;
