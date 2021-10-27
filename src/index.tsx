e06825fbdfce62b446c1f273efc7ea9c4bbbc96d
Kamaal <kamaal@tranzer.com>
2021-10-27 23:05:11 +0200

    fix: fixing unpacking native modules

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,12 @@ type PassLibraryType = {
   getRemotePKPassAndPresentPKPassView: (url: string) => Promise<void>;
 };
 
-const { PassLibrary } = NativeModules;
+const { RNPassLibrary } = NativeModules;
 
 const Package: PassLibraryType = {
   getRemotePKPassAndPresentPKPassView: (url) => {
     if (Platform.OS !== 'ios') throw 'This library is not supported on Android';
-    return PassLibrary.getRemotePKPassAndPresentPKPassView(url);
+    return RNPassLibrary.getRemotePKPassAndPresentPKPassView(url);
   },
 };
 
